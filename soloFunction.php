586354341b78e62e30096db8e14c1d7d6958d5f4
Erica Wright <erica.wright@soliddigital.com>
2017-09-07 15:51:40 -0700

    add file containing solo function to retrieve images and video for reuse

---
+++ soloFunction.php
@@ -0,0 +1,31 @@
+<?php
+    function retrieveMovieFolder(movFilePath) {
+        $videoFileEndings = ['.mov', '.avi', '.gif', '.mp4', '.webm'];
+        $folderPath = str_replace($videoFileEndings, '', movFilePath);
+        if (file_exists($folderPath)) {
+            retrieveFolderImages($folderPath);
+        } else {
+            try {
+                mkdir($folderPath);
+            } catch (Exception $e) {
+                `echo ${e}`;
+            }
+            try {
+                exec('ffmpeg -i ' . movFilePath . ' -vf fps=1/30 ' . $folderPath . '/img%03d.png');
+            } catch (Exception $e) {
+                `echo ${e}`;
+            }
+            retrieveFolderImages($folderPath);
+        }
+        return $images;
+    }
+
+    function retrieveFolderImages(folderPath) {
+        $images = [];
+        $allFolderFileNames = array_diff(scandir(folderPath), array('..', '.'));
+            foreach($allFolderFileNames as $fileName) {
+                if (pathinfo($fileName)['extension'] === 'jpg' || pathinfo($fileName)['extension'] === 'png') {
+                    array_push($images, $fileName);
+                }
+            }
+    }
